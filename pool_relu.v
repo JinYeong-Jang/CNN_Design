aea970c89b7fb841f40cfe93ac98a52d5bfb7b6c
wjdalswl-airair <162536914+wjdalswl-airair@users.noreply.github.com>
2025-08-25 23:01:26 +0900

    Update pool_relu.v

--- pool_relu.v
+++ pool_relu.v
@@ -6,9 +6,6 @@ module pool_relu #(
     input  wire                      clr,         // sync, active-high
     input  wire                      in_valid,    // 1 pixel / clk
     input  wire signed [In_d_W-1:0]  in_data,     // [31:0] signed pixel
-    input  wire signed [In_d_W-1:0]  a; //max-pooling input
-    input  wire signed [In_d_W-1:0]  b; //max-pooling input
-    input  wire signed [In_d_W-1:0]  x; //ReLU input
     
     output reg                       out_valid,
     output reg signed [In_d_W-1:0]   out_data // [31:0]
@@ -29,14 +26,21 @@ module pool_relu #(
     reg signed [In_d_W-1:0] even_s0;  // previous sample
     reg signed [In_d_W-1:0] even_s1;  // older sample
 
+    //max-pooling and relu input
+    wire signed [In_d_W-1:0]  a; //max-pooling input
+    wire signed [In_d_W-1:0]  b; //max-pooling input
+    wire signed [In_d_W-1:0]  x; //ReLU input
+    
     // Signed max and ReLU
     function [In_d_W-1:0] maxpool;
+        input signed [In_d_W-1:0] a, b;
         begin
             if (a >= b) maxpool = a; else maxpool = b;
         end
     endfunction
 
     function [In_d_W-1:0] relu;
+        input signed [In_d_W-1:0] x;
         begin
             if (x[In_d_W-1]) relu = {In_d_W{1'b0}}; else relu = x;
         end
@@ -111,4 +115,5 @@ module pool_relu #(
         end
     end
 endmodule
+
 
