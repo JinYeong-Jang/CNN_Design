ed3d53ffa7e05d5b31e2b5f14c3fa9b255636c8d
JinYeong <74659950+JinYeong-Jang@users.noreply.github.com>
2025-08-27 14:43:39 +0900

    Update conv3x3.v

--- conv3x3.v
+++ conv3x3.v
@@ -14,8 +14,8 @@ module conv3x3 #(
     input  signed [WI*9-1:0]      conv_weight,    // {w00,w01,...,w22}
     input  signed [BW-1:0]        conv_bias,         // bias for this output channel
 
-    output reg                    oOutValid,
-    output reg signed [ACCW-1:0]  oOutData
+    output                    oOutValid,
+    output  signed [ACCW-1:0]  oOutData
 );
 
     // ---------------------------------
@@ -29,110 +29,79 @@ module conv3x3 #(
     // ---------------------------------
     // weight/bias ROM
     // ---------------------------------
+    wire signed [DATAW-1:0] a00 = iWindowInRow1[3*DATAW-1 -: DATAW];
+    wire signed [DATAW-1:0] a01 = iWindowInRow1[2*DATAW-1 -: DATAW];
+    wire signed [DATAW-1:0] a02 = iWindowInRow1[1*DATAW-1 -: DATAW];
 
-    wire signed [DATAW-1:0] a00, a01, a02,
-                            a10, a11, a12,
-                            a20, a21, a22;
+    wire signed [DATAW-1:0] a10 = iWindowInRow2[3*DATAW-1 -: DATAW];
+    wire signed [DATAW-1:0] a11 = iWindowInRow2[2*DATAW-1 -: DATAW];
+    wire signed [DATAW-1:0] a12 = iWindowInRow2[1*DATAW-1 -: DATAW];
 
-    wire signed [WW-1:0] w00, w01, w02,
-                         w10, w11, w12,
-                         w20, w21, w22;
+    wire signed [DATAW-1:0] a20 = iWindowInRow3[3*DATAW-1 -: DATAW];
+    wire signed [DATAW-1:0] a21 = iWindowInRow3[2*DATAW-1 -: DATAW];
+    wire signed [DATAW-1:0] a22 = iWindowInRow3[1*DATAW-1 -: DATAW];
 
-    wire signed [BW-1:0] b;
+    wire signed [WW-1:0] w00 = conv_weight[(1)*WW-1 -: WW];
+    wire signed [WW-1:0] w01 = conv_weight[(2)*WW-1 -: WW];
+    wire signed [WW-1:0] w02 = conv_weight[(3)*WW-1 -: WW];
+    wire signed [WW-1:0] w10 = conv_weight[(4)*WW-1 -: WW];
+    wire signed [WW-1:0] w11 = conv_weight[(5)*WW-1 -: WW];
+    wire signed [WW-1:0] w12 = conv_weight[(6)*WW-1 -: WW];
+    wire signed [WW-1:0] w20 = conv_weight[(7)*WW-1 -: WW];
+    wire signed [WW-1:0] w21 = conv_weight[(8)*WW-1 -: WW];
+    wire signed [WW-1:0] w22 = conv_weight[(9)*WW-1 -: WW];
+    wire signed [BW-1:0] b    = conv_bias;
+
+    wire ce = iInValid;
+
 
     // product results
-    wire signed [PRODW-1:0] p00, p01, p02,
+    reg signed [PRODW-1:0] p00, p01, p02,
                                p10, p11, p12,
                                p20, p21, p22;
 
-    // sum results
-    wire signed [ACCW-1:0] sum_products;
-    wire signed [ACCW-1:0] sum_with_bias;
+    always @(posedge iClk) begin
+        if (!iRsn) begin
+            p00<=0; p01<=0; p02<=0; p10<=0; p11<=0; p12<=0; p20<=0; p21<=0; p22<=0;
+        end else if (ce) begin
+            p00 <= a00 * w00;  p01 <= a01 * w01;  p02 <= a02 * w02;
+            p10 <= a10 * w10;  p11 <= a11 * w11;  p12 <= a12 * w12;
+            p20 <= a20 * w20;  p21 <= a21 * w21;  p22 <= a22 * w22;
+        end
+    end
 
-    // base index(OC)
-    wire [7:0] base;
-
-    //level 1 wire & reg
-    wire signed [ACCW-1:0] s0_l1, s1_l1, s2_l1, s3_l1, s4_l1;
-    reg signed [ACCW-1:0] s0_r1, s1_r1, s2_r1, s3_r1, s4_r1;
-    reg                   v_r1;
-
-    //level 2 wire
-    wire signed [ACCW-1:0] s0_l2, s1_l2, s2_l2; 
-
-    //level 3 wire & reg
-    wire signed [ACCW-1:0] s0_l3, s1_l3;
-    reg signed [ACCW-1:0] s0_r3, s1_r3;
-    reg                   v_r3;
-
-
-    
-    // ---------------------------------
-    // input data assign
-    // ---------------------------------
-    assign a00 = iWindowInRow1[3*DATAW-1 -: DATAW];
-    assign a01 = iWindowInRow1[2*DATAW-1 -: DATAW];
-    assign a02 = iWindowInRow1[1*DATAW-1 -: DATAW];
-
-    assign a10 = iWindowInRow2[3*DATAW-1 -: DATAW];
-    assign a11 = iWindowInRow2[2*DATAW-1 -: DATAW];
-    assign a12 = iWindowInRow2[1*DATAW-1 -: DATAW];
-
-    assign a20 = iWindowInRow3[3*DATAW-1 -: DATAW];
-    assign a21 = iWindowInRow3[2*DATAW-1 -: DATAW];
-    assign a22 = iWindowInRow3[1*DATAW-1 -: DATAW];
-
-    // ---------------------------------
-    // weight/bias assign
-    // ---------------------------------
-    assign w00 =  conv_weight[(1)*WW-1 -: WW];
-    assign w01 =  conv_weight[(2)*WW-1 -: WW];
-    assign w02 =  conv_weight[(3)*WW-1 -: WW];
-    assign w10 =  conv_weight[(4)*WW-1 -: WW];
-    assign w11 =  conv_weight[(5)*WW-1 -: WW];
-    assign w12 =  conv_weight[(6)*WW-1 -: WW];
-    assign w20 =  conv_weight[(7)*WW-1 -: WW];
-    assign w21 =  conv_weight[(8)*WW-1 -: WW];
-    assign w22 =  conv_weight[(9)*WW-1 -: WW];
-
-    // ---------------------------------
-    // mul instance
-    // ---------------------------------
-    mul #(.WI(WI)) u_mul_00 (.w(w00), .x(a00), .y(p00));
-    mul #(.WI(WI)) u_mul_01 (.w(w01), .x(a01), .y(p01));
-    mul #(.WI(WI)) u_mul_02 (.w(w02), .x(a02), .y(p02));
-
-    mul #(.WI(WI)) u_mul_10 (.w(w10), .x(a10), .y(p10));
-    mul #(.WI(WI)) u_mul_11 (.w(w11), .x(a11), .y(p11));
-    mul #(.WI(WI)) u_mul_12 (.w(w12), .x(a12), .y(p12));
-
-    mul #(.WI(WI)) u_mul_20 (.w(w20), .x(a20), .y(p20));
-    mul #(.WI(WI)) u_mul_21 (.w(w21), .x(a21), .y(p21));
-    mul #(.WI(WI)) u_mul_22 (.w(w22), .x(a22), .y(p22));
-
- // sign-extend helper
+    // sign-extend helper
     function [ACCW-1:0] sx;
         input signed [PRODW-1:0] v;
         begin sx = {{(ACCW-PRODW){v[PRODW-1]}}, v}; end
     endfunction
 
     // 한 번에 조합 합산
-    wire signed [ACCW-1:0] sum_products =
+    wire signed [ACCW-1:0] sum_products;
+    assign sum_products =
           sx(p00)+sx(p01)+sx(p02)
         + sx(p10)+sx(p11)+sx(p12)
         + sx(p20)+sx(p21)+sx(p22);
 
-    wire signed [ACCW-1:0] sum_with_bias =
-        sum_products + {{(ACCW-BW){conv_bias[BW-1]}}, conv_bias};
+    wire signed [ACCW-1:0] sum_with_bias;
+    assign sum_with_bias =
+        sum_products + {{(ACCW-BW){b[BW-1]}}, b};
 
-    // 출력 1단 레지스터 → LAT_kernel = 1
+    reg                      rOutValid;
+    reg  signed [ACCW-1:0]   rOutData;
     always @(posedge iClk) begin
         if (!iRsn) begin
-            oOutValid <= 1'b0;
-            oOutData  <= {ACCW{1'b0}};
+            rOutValid <= 1'b0;
+            rOutData  <= {ACCW{1'b0}};
+        end else if (ce) begin
+            rOutValid <= 1'b1;
+            rOutData  <= sum_with_bias;
         end else begin
-            oOutValid <= iInValid;      // 같은 단계로 valid 동행
-            oOutData  <= sum_with_bias; // data
+            rOutValid <= 1'b0;  // 필요하면 hold로 바꿔도 됨
         end
     end
+
+    assign oOutValid = rOutValid;
+    assign oOutData = rOutData;
+
 endmodule
